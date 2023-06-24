178dad157785fc2cd51916a6ec1746c7dfe35a3a
Zakaria Ismail <1112zakaria@gmail.com>
2023-06-23 20:59:05 -0400

    [LC-101] Symmetric Tree

---
+++ trees/101.symmetric-tree.cpp
@@ -0,0 +1,61 @@
+/*
+ * @lc app=leetcode id=101 lang=cpp
+ *
+ * [101] Symmetric Tree
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#if 0
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+#endif
+
+class Solution {
+public:
+    /**
+     * Approach:
+     * - maintain two root pointers A (left) and B (right)
+     * - A.left and B.right subtrees must be equal
+     * - A.right and B.left subtrees must be equal
+     * - base case: roots are NULL
+     * - recursive case:
+    */
+    bool isSymmetric(TreeNode* root) {
+        return areRootsMirrored(root->left, root->right);
+    }
+
+    bool areRootsMirrored(TreeNode* rootA, TreeNode* rootB) {
+        if (rootA == nullptr && rootB == nullptr) {
+            return true;
+        }
+        if (rootA == nullptr || rootB == nullptr) {
+            return false;
+        }
+
+        if (rootA->val == rootB->val) {
+            return areRootsMirrored(rootA->left, rootB->right) && 
+                areRootsMirrored(rootA->right, rootB->left);
+        }
+        return false; 
+    }
+};
+// @lc code=end
+
